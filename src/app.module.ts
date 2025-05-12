2d8734a3e098fc583e633a0b2383632bd6d05072
geowar17 <gjlalmlh@gmail.com>
2025-05-12 11:07:41 -0400

    proyecto en nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+
+import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projets.module';
+import { UsersModule } from './users/users.module';
+import { ProjectsController } from './projects/projects.controller';
+import { TasksController } from './tasks/tasks.controller';
+
+@Module({
+  imports: [TasksModule, ProjectsModule, UsersModule],
+  controllers: [TasksController, ProjectsController],
+  
+})
+export class AppModule {}
